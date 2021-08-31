31bbe4cbeae3f5ede12ca0e3ede3ace9f79d1264
Simon <simon.ess.se@gmail.com>
2021-08-31 22:00:48 +0200

    all models

--- Weather/Models.cs
+++ Weather/Models.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace Weather
 {
-    public class Content
+    public class Models
     {
-        public Cord cordinates { get; set; }
+        public Cord cordinations { get; set; }
         public Weather weather { get; set; }
         public string baseone { get; set; }
         public Main main { get; set; }
-        public int visibility { get; set; }
+        public int visbility { get; set; }
         public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
     }
 }
---
+++ Weather/Sys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+   public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public double message { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+}
