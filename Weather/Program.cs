792a6191136e1e3912b66199789c111976587e3f
Simon <simon.ess.se@gmail.com>
2021-09-16 21:23:26 +0200

    some changes

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -11,8 +11,6 @@ namespace Weather
 {
     public class Program
     {
-        static readonly HttpClient client = new HttpClient();
-
         static async Task<int> Main(string[] args)
         {
             return await Parser.Default.ParseArguments<CommandLineOptions>(args)
@@ -38,35 +36,6 @@ namespace Weather
                         });
                 }).UseWindowsService();
 
-        static async Task GetWeather()
-        {
-
-           var locator = new Windows.Devices.Geolocation.Geolocator();
-
-            var position = (await locator.GetGeopositionAsync()).Coordinate.Point.Position;
-
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={position.Latitude}&lon={position.Longitude}&appid=171dd6e6b0fe8d04a0fd21a1a4330d49&units=metric");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Models m = JsonConvert.DeserializeObject<Models>(responseBody);
-
-
-
-                Console.WriteLine($"Ort: {m.Name}, Tempmax: {m.main.TempMax}, Tempmin: {m.main.TempMin}");
-            }
-
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-
-            }
-
-
-
-        }
         
     }
 
--- Weather/WeatherClassifierWorker.cs
+++ Weather/WeatherClassifierWorker.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ namespace Weather
 {
    public class WeatherClassifierWorker : BackgroundService
     {
+        static readonly HttpClient client = new HttpClient();
         private SqueezeNetModel _squeezeNetModel;
         private readonly List<string> _labels = new List<string>();
 
@@ -21,6 +24,36 @@ namespace Weather
         {
             _logger = logger;
             _options = options;
+        }
+
+        static async Task GetWeather()
+        {
+
+            var locator = new Windows.Devices.Geolocation.Geolocator();
+
+            var position = (await locator.GetGeopositionAsync()).Coordinate.Point.Position;
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={position.Latitude}&lon={position.Longitude}&appid=171dd6e6b0fe8d04a0fd21a1a4330d49&units=metric");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Models m = JsonConvert.DeserializeObject<Models>(responseBody);
+
+
+
+                Console.WriteLine($"Ort: {m.Name}, Tempmax: {m.main.TempMax}, Tempmin: {m.main.TempMin}");
+            }
+
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+
+            }
+
+
+
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
