d666c4061792df2805ff5e0ddd811cfe4ca114b0
Simon <simon.ess.se@gmail.com>
2021-09-23 19:30:27 +0200

    get geolocation again

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -13,26 +13,21 @@ namespace Weather
     {
         static async Task<int> Main(string[] args)
         {
-            return await Parser.Default.ParseArguments<CommandLineOptions>(args)
-                .MapResult(async (opts) =>
-                {
-                    await CreateHostBuilder(args, opts).Build().RunAsync();
-                    return 0;
-                },
-                errs => Task.FromResult(-1)); // Invalid arguments
+
+            await CreateHostBuilder(args).Build().RunAsync();
+            return 0;
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args, CommandLineOptions opts) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(configureLogging => configureLogging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Information))
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddSingleton(opts);
-                    services.AddHostedService<ImageClassifierWorker>()
+                    services.AddHostedService<WeatherClassifierWorker>()
                         .Configure<EventLogSettings>(config =>
                         {
-                            config.LogName = "Image Classifier Service";
-                            config.SourceName = "Image Classifier Service Source";
+                            config.LogName = "Weather Classifier Service";
+                            config.SourceName = "Weather Classifier Service Source";
                         });
                 }).UseWindowsService();
 
--- Weather/WeatherClassifierWorker.cs
+++ Weather/WeatherClassifierWorker.cs
@@ -15,16 +15,15 @@ namespace Weather
 {
     public class WeatherClassifierWorker : BackgroundService
     {
-        private SqueezeNetModel _squeezeNetModel;
-        private readonly List<string> _labels = new List<string>();
 
         private readonly ILogger<WeatherClassifierWorker> _logger;
-        private readonly CommandLineOptions _options;
 
-        public WeatherClassifierWorker(ILogger<WeatherClassifierWorker> logger, CommandLineOptions options)
+
+        static readonly HttpClient client = new HttpClient();
+
+        public WeatherClassifierWorker(ILogger<WeatherClassifierWorker> logger)
         {
             _logger = logger;
-            _options = options;
         }
 
 
@@ -34,76 +33,6 @@ namespace Weather
 
             _logger.LogInformation("Service started");
 
-            if (!Directory.Exists(_options.Path))
-            {
-                _logger.LogError($"Directory "{ _options.Path}
-                " does not exist.");
-                return;
-            }
-
-            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-            // Load labels from JSON
-            foreach (var kvp in JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(Path.Combine(rootDir, "Labels.json"))))
-            {
-                _labels.Add(kvp.Value);
-            }
-
-            _squeezeNetModel = SqueezeNetModel.CreateFromFilePath(Path.Combine(rootDir, "squeezenet1.0-9.onnx"));
-
-
-            _logger.LogInformation($"Listening for images created in "{ _options.Path}
-            "...");
-
-            using FileSystemWatcher watcher = new FileSystemWatcher
-            {
-                Path = _options.Path
-            };
-            foreach (var extension in _options.Extensions)
-            {
-                watcher.Filters.Add($"*.{extension}");
-            }
-            watcher.Created += async (object sender, FileSystemEventArgs e) =>
-            {
-                await Task.Delay(1000);
-                await ProcessFileAsync(e.FullPath, _options.Confidence);
-            };
-
-            watcher.EnableRaisingEvents = true;
-
-
-            var tcs = new TaskCompletionSource<bool>();
-            stoppingToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
-            await tcs.Task;
-
-            _logger.LogInformation("Service stopped");
-
-            if (results[0].p >= confidence)
-            {
-                MoveFileToFolder(filePath, _labels[results[0].index]);
-
-                return true; // Success
-            }
-
-            return false; // Not enough confidence or error
-
-
-        }
-
-        private void MoveFileToFolder(string filePath, string folderName)
-        {
-            var directory = Path.GetDirectoryName(filePath);
-            var fileName = Path.GetFileName(filePath);
-            var destinationDirectory = Path.Combine(directory, folderName);
-
-            Directory.CreateDirectory(destinationDirectory);
-
-            File.Move(filePath, Path.Combine(destinationDirectory, fileName), false);
-        }
-
-
-        public static async Task GetWeather()
-        {
 
             var locator = new Windows.Devices.Geolocation.Geolocator();
 
@@ -127,7 +56,25 @@ namespace Weather
                 Console.WriteLine("Message :{0} ", e.Message);
 
             }
+
+
+
+            var tcs = new TaskCompletionSource<bool>();
+            stoppingToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            await tcs.Task;
+
+            _logger.LogInformation("Service stopped");
+
+            
+
         }
+
+
+        //public static async Task GetWeather()
+        //{
+
+            
+        //}
 
     }
 }
